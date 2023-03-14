959a7a5077173dea78b9db1daaee6d0c5a0e14ee
2h311 <kcodepal@outlook.com>
2023-03-14 13:11:52 -0500

    made adjustments to the folder structure of the app

--- webapp/main.py
+++ webapp/main.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="../static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-templates = Jinja2Templates(directory="../templates")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
